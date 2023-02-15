9f9562a7d464aa2c4cd60a81f5a24d73b878171f
Jtboone1 <boojarrod@gmail.com>
2023-02-15 19:12:49 -0330

    Added function to pass along message

--- pgn.h
+++ pgn.h
@@ -4,9 +4,9 @@
 #include <SPI.h>
 #include <mcp2515.h>
 
-#define PGN_IS_ACKNOWLEDGEMENT 059392
-#define PGN_ISO_REQUEST        055904
-#define PGN_ISO_ADDRESS_CLAIM  060928
+#define PGN_IS_ACKNOWLEDGEMENT 59392
+#define PGN_ISO_REQUEST        55904
+#define PGN_ISO_ADDRESS_CLAIM  60928
 #define PGN_REQUEST_GRP_FUNC   126208
 #define PGN_TRANSMIT_GRP_FUNC  126464
 #define PGN_SYSTEM_TIME        126992
@@ -20,5 +20,7 @@
 #define PGN_GNSS_POS           129029
 #define PGN_GNSS_DOP           129539
 #define PGN_GNSS_SATS_VIEW     129539
+
+void pgn_processor(int PGN, can_frame recvMsg);
 
 void print_pgn(struct can_frame recvMsg);
--- pgn.ino
+++ pgn.ino
@@ -1,5 +1,14 @@
 #include "pgn.h"
 
+void pgn_processor(int PGN, can_frame recvMsg)
+{
+    // If we get the default PGN's we'll pass on the message.
+    if (PGN == PGN_IS_ACKNOWLEDGEMENT || PGN == PGN_ISO_ADDRESS_CLAIM || PGN == PGN_ISO_REQUEST)
+    {
+        print_pgn(recvMsg);
+    }
+}
+
 void print_pgn(struct can_frame recvMsg)
 {
     Serial.println("ID  DLC  DATA");
